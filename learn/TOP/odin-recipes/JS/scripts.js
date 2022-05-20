0ed30df8ab8015d780f7c2920f1008d8c4d55185
GreyBreadDoer <ips.qsf08@gmail.com>
2022-05-20 06:42:41 +0530

    JS lesson 1 mid way

---
+++ learn/TOP/odin-recipes/JS/scripts.js
@@ -0,0 +1,7 @@
+/* Basic variable declaration, concatenation and alert() function */
+
+let userName = "Debajyoti"
+    , penName = "GreyBeardDoer"
+    , currentProject = "The Odin Project";
+
+alert("The pen name of " + userName + " is " + penName + ". He is currently working on " + currentProject)
