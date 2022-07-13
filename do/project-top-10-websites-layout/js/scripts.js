2564490c19d502c1a6c1dad68c1f43675cf4f191
GreyBeardDoer <ips.qsf08@gmail.com>
2022-07-13 21:10:04 +0530

    DO folder restarted with Brad Traversy projects

--- do/project-top-10-websites-layout/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function displayOnClick() {
-    const message = "THIS IS A DUMMY WEBSITE. IT IS CREATED FOR LEARNING PURPOSE ONLY.";
-    alert(message);
-}
-
-const userClicked = document.querySelector('body');
-userClicked.addEventListener('click', displayOnClick);
---
+++ learn/script.js
@@ -0,0 +1,11 @@
+let myName="Debajyoti";
+let shortName;
+shortName = myName.slice(4);
+console.log(myName);
+console.log(shortName);
+
+myName = myName.replace("jyoti", "lina");
+console.log(myName);
+
+shortName = myName.slice(4);
+console.log(shortName);
