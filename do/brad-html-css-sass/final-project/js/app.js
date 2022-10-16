c121a837eea2e07cbb41f7565bbbe502b3099e62
GreyBeardDoer <ips.qsf08@gmail.com>
2022-10-16 14:42:29 +0530

    Toggling navbar completed

--- do/brad-html-css-sass/final-project/js/app.js
+++ do/brad-html-css-sass/final-project/js/app.js
@@ -1,3 +1,18 @@
-const nav_toggle = document.querySelector('nav');
 
-// nav_toggle.style.color="green";
+document.querySelector('.toggle-btn').addEventListener('click', nav_toggler);
+
+const nav_toggle = document.querySelector('nav');
+let nav_state = "off";
+
+function nav_toggler(){
+  
+  if(nav_state === "off") {
+    nav_toggle.style.display = 'block';
+    nav_state = "on";
+  }else if(nav_state === "on"){
+    nav_toggle.style.display = 'none';
+    nav_state = "off";
+  }
+
+}
+
