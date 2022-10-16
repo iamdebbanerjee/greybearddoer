348be7782b7b9fb86e9d5f8a89daac4af925da21
GreyBeardDoer <ips.qsf08@gmail.com>
2022-10-16 21:46:12 +0530

    Crude but working nav animation

--- do/brad-html-css-sass/final-project/js/app.js
+++ do/brad-html-css-sass/final-project/js/app.js
@@ -6,13 +6,15 @@ let nav_state = "off";
 
 function nav_toggler(){
   
-  if(nav_state === "off") {
-    nav_toggle.style.display = 'block';
+    if(nav_state === "off") {
+    nav_toggle.style.height = '50vh';
+    nav_toggle.style.visibility = 'visible';
     nav_state = "on";
-  }else if(nav_state === "on"){
-    nav_toggle.style.display = 'none';
+    }else if(nav_state === "on"){
+    nav_toggle.style.height = '0';
+    nav_toggle.style.visibility = 'hidden';
     nav_state = "off";
-  }
+    }
 
 }
 
