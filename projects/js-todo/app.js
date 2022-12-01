bb62aae1155236ee7955de95cd574215fd1b46ae
Quantophilic <ips.qsf08@gmail.com>
2022-12-01 12:44:27 +0530

    JS UI Variables declared

---
+++ projects/js-todo/app.js
@@ -0,0 +1,15 @@
+// Declare the variables
+
+const taskForm = document.getElementById('task-form');
+const addTask = document.querySelector('.task-add-btn');
+const taskFilter = document.querySelector('#filter-task')
+const taskList = document.querySelector('.task-list');
+const clearTasks = document.querySelector('.task-clr-btn')
+
+
+
+
+
+// Program Test
+
+console.log(taskForm, addTask, taskFilter, taskList, clearTasks);
