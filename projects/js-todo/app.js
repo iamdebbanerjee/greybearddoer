c05a76410cb7931cc600e036f74d617d36d321f7
GreyBeardDoer <ips.qsf08@gmail.com>
2022-12-02 22:10:31 +0530

    Minor fixes.

--- projects/js-todo/app.js
+++ projects/js-todo/app.js
@@ -17,52 +17,53 @@ function loadEventListeners(){
 	// Add Task event
 	taskForm.addEventListener('submit', taskAdd);
 
+}
+
 	// Task Add Function
-	function taskAdd(e){
-		// Check if the user has entered anything or not
-		if (taskInput.value === ''){
-			alert("Please enter a Task");
-		}
-			
-		// Create new task
-		const newTask = taskInput.value;
-
-
-		// Create new List Item element
-		let newListItem = document.createElement('li');
-
-		// Create new text node
-		let newTextNode = document.createTextNode(newTask);
-    
-    	// Append newTextNode to newListItem
-		newListItem.appendChild(newTextNode);
-
-		// Create new a element
-		let newDeleteLink = document.createElement('a');
-
-		// Create new Font Awesome x icon for deleting tasks
-		let newDeleteIcon = document.createElement('i');
-
-		// Assign fa, fa-remove classes to the x icon
-		newDeleteIcon.classList.add('fa');
-    	newDeleteIcon.classList.add('fa-remove');
-
-		// Append icon to a element as child
-		newDeleteLink.appendChild(newDeleteIcon);
-
-		// Give a element the href = # and class = 'delete-task' attributes
-		newDeleteLink.setAttribute('href', '#');
-		newDeleteLink.classList.add('delete-task');
-
-		// Append a element to parent node of newTextNode (since txt nodes cannot append child nodes)
-		newTextNode.parentElement.appendChild(newDeleteLink);
-
-		// Append newListItem to taskList
-		taskList.appendChild(newListItem);
-
-
-		e.preventDefault();
+function taskAdd(e){
+	// Check if the user has entered anything or not
+	if (taskInput.value === ''){
+		alert("Please enter a Task");
 	}
+		
+	// Create new task
+	const newTask = taskInput.value;
+
+
+	// Create new List Item element
+	let newListItem = document.createElement('li');
+
+	// Create new text node
+	let newTextNode = document.createTextNode(newTask);
+
+	// Append newTextNode to newListItem
+	newListItem.appendChild(newTextNode);
+
+	// Create new a element
+	let newDeleteLink = document.createElement('a');
+
+	// Create new Font Awesome x icon for deleting tasks
+	let newDeleteIcon = document.createElement('i');
+
+	// Assign fa, fa-remove classes to the x icon
+	newDeleteIcon.classList.add('fa');
+	newDeleteIcon.classList.add('fa-remove');
+
+	// Append icon to a element as child
+	newDeleteLink.appendChild(newDeleteIcon);
+
+	// Give a element the href = # and class = 'delete-task' attributes
+	newDeleteLink.setAttribute('href', '#');
+	newDeleteLink.classList.add('delete-task');
+
+	// Append a element to parent node of newTextNode (since txt nodes cannot append child nodes)
+	newTextNode.parentElement.appendChild(newDeleteLink);
+
+	// Append newListItem to taskList
+	taskList.appendChild(newListItem);
+
+
+	e.preventDefault();
 }
 
 
