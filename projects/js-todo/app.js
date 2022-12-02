925a9f8e9ed0a1a2925a1003dea48fc10b8e6b85
Quantophilic <ips.qsf08@gmail.com>
2022-12-02 11:15:58 +0530

    New task addition function complete

--- projects/js-todo/app.js
+++ projects/js-todo/app.js
@@ -27,11 +27,37 @@ function loadEventListeners(){
 		// Create new task
 		const newTask = taskInput.value;
 
-		// Create new List Item element and assign this task
 
+		// Create new List Item element
 		let newListItem = document.createElement('li');
+
+		// Create new text node
 		let newTextNode = document.createTextNode(newTask);
+    
+    // Append newTextNode to newListItem
 		newListItem.appendChild(newTextNode);
+
+		// Create new a element
+		let newDeleteLink = document.createElement('a');
+
+		// Create new Font Awesome x icon for deleting tasks
+		let newDeleteIcon = document.createElement('i');
+
+		// Assign fa, fa-remove classes to the x icon
+		newDeleteIcon.classList.add('fa');
+    	newDeleteIcon.classList.add('fa-remove');
+
+		// Append icon to a element as child
+		newDeleteLink.appendChild(newDeleteIcon);
+
+		// Give a element the href = # and class = 'delete-task' attributes
+		newDeleteLink.setAttribute('href', '#');
+		newDeleteLink.classList.add('delete-task');
+
+		// Append a element to parent node of newTextNode (since txt nodes cannot append child nodes)
+		newTextNode.parentElement.appendChild(newDeleteLink);
+
+		// Append newListItem to taskList
 		taskList.appendChild(newListItem);
 
 
